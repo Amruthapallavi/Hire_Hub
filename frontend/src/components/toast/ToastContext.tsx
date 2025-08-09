7f090d9a5319a3b932812ac47db95fbf8323e841
Amritha <169130861+Amruthapallavi@users.noreply.github.com>
2025-08-10 03:21:59 +0530

    unused components removed

--- frontend/src/components/toast/ToastContext.tsx
+++ frontend/src/components/toast/ToastContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import  { createContext, useState } from "react";
 import type { ReactNode } from "react";
 
 import type { Toast, ToastType, ToastAction } from "../../types/types";
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 import tailwindcss from '@tailwindcss/vite'
